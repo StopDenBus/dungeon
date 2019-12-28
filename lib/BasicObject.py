4c7734f68e75675e0fb4209aefa1b32f4a00c8f1
Michael Gusek <michael.gusek@web.de>
2019-12-28 13:36:22 +0100

    requirements.txt created

--- lib/BasicObject.py
+++ lib/BasicObject.py
@@ -8,73 +8,132 @@ sys.path.append('../')
 from lib.constants import *
 
 class BasicObject():
-
+	"""
+	Basic object holds properties shared will all other objects
+	"""
 	def __init__(self):
 
 		super().__init__()
 
+		# Deprecated
+		# List of commands that are assigned to this object
 		self.__commands = { }
 
+		# Objects can be 'in' another ojects, e.g. inventory for players
+		# saves the 'parent' object
 		self.__container = None
 
+		# long description of an object
 		self.__description = None
 
+		# gender of an object
 		self.__gender = None
 
+		# Deprecated
+		# the name of an object or player
+		# just use __name
 		self.__id = None
 
+		# list of id's
 		self.__identities = [ ]
 
+		# the name of an object, player name, npc name
+		self.__name = None
+
+		# the name of an object if there is more than one of them in a stack
+		# e.g. 'Banane'  -> 'Bananen'
 		self.__plural = None
 
+		# each object can have their own additional properties
 		self.__properties = { }
 
+		# short description of an object
 		self.__short_description = None
 
+		# the name of an object if there is only one of them in a stack
+		# e.g. 'Bananen'  -> 'Banane'
 		self.__singular = None
 
+		# the value of an object
 		self.__value = 0
 
+		# the weight of an object
 		self.__weight = 0
 
 	def getDescription(self):
-
+		"""
+		Returns the description of an object
+			:param self: the object itself
+		"""
 		return self.__description
 
 	def setDescription(self, description):
-
+		"""
+		Sets the description of an object.
+			:param self: the object itself
+			:param description: the description to set
+		"""
 		self.__description = description
 
 	def addProperty(self, key, value):
-
+		"""
+		Adds a property to an object
+			:param self: the object itself
+			:param key: the key of the property
+			:param value: the value of the property, can be single value, list, dictionary, ...
+		"""
 		self.__properties[key] = value
 
 	def getProperty(self, key):
-
+		"""
+		Returns the value a given key, return 'None' if key not exists
+			:param self: the object itself
+			:param key: the key of interest
+		"""
+		# check if key exists
 		if key in self.__properties.keys():
 
+			# return value
 			return self.__properties[key]
 
+		# key not found, return 'None'
 		return None
 
-	def setName(self, name):
-
-		self.__name = name
-
-		self.__id = name
-
-		self.addIdentity(name)
-
 	def getName(self):
-
+		"""
+		Returns the capitalized name of an object
+			:param self: the object itself
+		"""
 		return self.__name.capitalize()
 
-	def getWeight(self):
+	def setName(self, name):
+		"""
+		Sets the name of an object
+			:param self: the object itself
+			:param name: the name to set
+		"""
+		# set the name
+		self.__name = name
 
+		# depricated: set the id
+		self.__id = name
+
+		# add name to list of id's
+		self.addIdentity(name)
+
+	def getWeight(self):
+		"""
+		Returns the weight of an object
+			:param self: the object itself
+		"""
 		return self.__weight
 
 	def setWeight(self, weight):
-
+		"""
+		Sets the weight of an object
+			:param self: the object itself
+			:param weight: the weight to set
+		"""
 		self.__weight = weight
 
 	def getShortDescription(self):
